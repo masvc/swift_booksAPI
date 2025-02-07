d14b2148cac26860056a16431e45fc6007b733b5
mas <m.yoshida553@gmail.com>
2025-02-07 19:18:12 +0900

    Initial Commit

---
+++ BookApp/BookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookAppApp.swift
+//  BookApp
+//
+//  Created by masato yoshida on 2025/02/07.
+//
+
+import SwiftUI
+
+@main
+struct BookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
